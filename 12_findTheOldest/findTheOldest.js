94c36443a20f2657b7c0d42d3c5271c100682407
Stephen M <stephen.domingo@outlook.com>
2022-12-04 19:55:49 -0800

    complete 12_findTheOldest FINALLY

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,29 +1,24 @@
 const findTheOldest = function(info) {
+    
+    let name = "";
+    let age = 0;
+    let i = 0;
+    let dateNow = 2022;
+    
+    while(i < info.length) {
+    
+        let newAge = (info[i].yearOfDeath || dateNow) - info[i].yearOfBirth;
+        
+        if (newAge > age) {
+            name = info[i].name;
+            age = newAge;
+        }
 
-/*
-1. grab age, and if it is larget than the previous, keep the name
-2. return the name
-
-1. search through array and pull the dates of birth and death
-2. compare the ages, keep the highest age
-3. return the name of the person with the highest age
-*/
-
-let name = "";
-let age = 0;
-
-while (i = 0, i < info.length, i++) {
-    let newAge = info.yearOfDeath - info.yearOfBirth;
-    if (newAge > age) {
-        name = info.name;
-        newAge = age;
+        i++
     }
-    console.log(name);
-    console.log(age);
-}
-
-return name;
-}
+        
+    return name;
+};
 
 // Do not edit below this line
 module.exports = findTheOldest;
