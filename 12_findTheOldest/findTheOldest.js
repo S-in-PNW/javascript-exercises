c97904113a77f33ea4a0d76ee3b2ad3a2e91ed28
Stephen M <stephen.domingo@outlook.com>
2022-12-04 18:40:21 -0800

    slowly getting there

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,39 @@
-const findTheOldest = function() {
+const findTheOldest = function(info) {
 
+
+/*
+1. search through array and pull the dates of birth and death
+2. compare the ages, keep the highest age
+3. return the name of the person with the highest age
+*/
+
+let age = info.find(function dead (years) {
+    let age = years[yearOfDeath] - years[yearOfBirth];
+    return age;
+});
+
+while (let i = 0; i < info.length; i++) {
+    if ()
+}
+
+
+//function to get age of living ppl
+function living (years){
+    let age = Date.prototype.getFullYear() - years[yearOfBirth];
+    return age;
+};
+
+//function to get age at death
+
+
+//function to compare ages
+function compare (birth, death) {
+    if 
+}
+
+return years(info);
+
+    
 };
 
 // Do not edit below this line
